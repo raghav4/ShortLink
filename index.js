7b7742cf0cd3b7edaa7bcaf771fb17ad4f434a44
Raghav Sharma <raghavsyt@gmail.com>
2020-02-08 11:57:56 +0530

    refactored code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 // Builtin Middlewares
@@ -12,14 +11,8 @@ app.use('/static', express.static(__dirname + '/static'));
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
-// routes
 require('./startup/routes')(app);
-
-mongoose.connect('mongodb://localhost/url-shortener', {
-        useNewUrlParser: true
-    })
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Failed to connect to MongoDB...'))
+require('./startup/db')();
 
 const PORT = process.env.PORT || 3000;
 app.listen((PORT), () => console.log(`Listening on Port ${PORT}...`));
---
+++ startup/db.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+require('dotenv').config()
+const mongoose = require('mongoose');
+
+module.exports = function () {
+    const dbURI = process.env.DB_URI;
+    mongoose.connect(dbURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(() => console.log(`Connected to MongoDB, URL = ${dbURI}...`))
+        .catch(err => console.error('Failed to connect to MongoDB...'))
+}
